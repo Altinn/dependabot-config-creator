5985354b22e0569d353616e49e0e3365dffe461c
HansO <hohagen@gmail.com>
2020-07-02 16:08:17 +0200

    Remove commented lines

--- Scanner.cs
+++ Scanner.cs
@@ -12,28 +12,6 @@ namespace DependabotConfigCreator
     {
         public static string[] DefaultIgnorePaths = new[] { @"\node_modules\", @"\bin\", "test" };
 
-        // public IEnumerable<string> GetCsProjects(string startDirectory, bool recursive)
-        // {
-        //     return GetDirectoryWithFiles(startDirectory, "*.csproj", DefaultIgnorePaths, recursive);
-        // }
-
-        // public IEnumerable<string> GetDockerProjects(string startDirectory, bool recursive)
-        // {
-        //     return GetDirectoryWithFiles(startDirectory, "Dockerfile", DefaultIgnorePaths, recursive);
-        // }
-
-        // public IEnumerable<string> GetMavenProjects(string startDirectory, bool recursive)
-        // {
-        //     return GetDirectoryWithFiles(startDirectory, "pom.xml", DefaultIgnorePaths, recursive);
-        // }
-
-
-        // public IEnumerable<string> GetNpmProjects(string startDirectory, bool recursive)
-        // {
-        //     return GetDirectoryWithFiles(startDirectory, "package.json", DefaultIgnorePaths, recursive);
-        // }
-
-
         public static IEnumerable<string> GetDirectoryWithFiles(string startDirectory, string filePattern, string[] ignorePaths, bool recursive)
         {
             return System.IO.Directory.GetFiles(startDirectory, filePattern, recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
